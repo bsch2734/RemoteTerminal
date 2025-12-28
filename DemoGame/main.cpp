06b21fbb4c9bff47945d29567e51bca5b4dbb90b
bsch2734 <bsch2734@yahoo.com>
2025-12-27 23:55:14 -0600

    add a project showing/ testing the usage of the engine

---
+++ DemoGame/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+#include "BattleshipEngine.h"
+
+int main() {
+	BattleshipEngine e;
+
+	for (const Ship& s : e.getFleetForPlayer(Player::one).getShips()) {
+		PlaceShipResult r;
+		do {
+			int rotation = rand() % 4;
+			coord pos = { { rand() % 10, rand() % 10 } };
+			r = e.placeShip(Player::one, s.getID(), pos, rotation);
+		} while (!r.success);
+	}
+
+	for (const Ship& s : e.getFleetForPlayer(Player::two).getShips()) {
+		PlaceShipResult r;
+		do {
+			int rotation = rand() % 4;
+			coord pos = { { rand() % 10, rand() % 10 } };
+			r = e.placeShip(Player::two, s.getID(), pos, rotation);
+		} while (!r.success);
+	}
+}
