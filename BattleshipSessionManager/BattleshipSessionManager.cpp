ba30039308d7b40e30c3debc2e3ec2293ab3bc01
bsch2734 <bsch2734@yahoo.com>
2026-01-03 16:14:38 -0600

    add BattleshipSessionManager library to keep track of in-progress and lobby games

---
+++ BattleshipSessionManager/BattleshipSessionManager.cpp
@@ -0,0 +1,53 @@
+#include "BattleshipSessionManager.h"
+
+AddUserToGameResult BattleshipSessionManager::addUserToGame(UserId u, GameId g) {
+	AddUserToGameResult answer;
+
+	//game is already in progress
+	auto inProgressGame = _gameIdToSessionMap.find(g);
+	if (inProgressGame != _gameIdToSessionMap.end()) {
+		answer.success = false;
+		answer.error = AddUserToGameError::gameFull;
+		answer.readyToPlay = true;
+		return answer;
+	}
+
+	auto lobbyGame = _lobbyGames.find(g);
+	//game does not exist yet
+	if (lobbyGame == _lobbyGames.end()) {
+		_lobbyGames.insert({ g, u });
+		answer.readyToPlay = false;
+		answer.success = true;
+		return answer;
+	}
+
+	//game exists only in lobby
+	//...and this user is trying to join twice
+	if (lobbyGame->second == u) {
+		answer.success = false;
+		answer.error = AddUserToGameError::userAlreadyInGame;
+		return answer;
+	}
+
+	//...and this is the second user
+	BattleshipSession* s = new BattleshipSession(g, lobbyGame->second, u);
+	_gameIdToSessionMap[g] = s;
+	_lobbyGames.erase(lobbyGame);
+	
+	answer.success = true;
+	answer.readyToPlay = true;
+	
+	return answer;
+}
+
+void BattleshipSessionManager::destroySession(GameId g) {
+	delete _gameIdToSessionMap[g];
+	_gameIdToSessionMap.erase(g);
+}
+
+BattleshipSession* BattleshipSessionManager::findSession(GameId g) {
+	auto f = _gameIdToSessionMap.find(g);
+	if (f == _gameIdToSessionMap.end())
+		return nullptr;
+	return (*f).second;
+}
---
+++ BattleshipSessionManager/BattleshipSessionManager.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "BattleshipSession.h"
+#include <map>
+
+enum class AddUserToGameError {
+	userAlreadyInGame,
+	gameFull
+};
+
+struct AddUserToGameResult {
+	bool success = false;
+	bool readyToPlay = false;
+	AddUserToGameError error;
+};
+
+class BattleshipSessionManager {
+public:
+	AddUserToGameResult addUserToGame(UserId u, GameId g);
+	void destroySession(GameId g);
+
+	BattleshipSession* findSession(GameId g);
+
+private:
+	std::map<GameId, BattleshipSession*> _gameIdToSessionMap;
+	std::map<GameId, UserId> _lobbyGames;
+};
