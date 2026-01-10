41938613c631e32ee59a9c160b7592853204d883
bsch2734 <bsch2734@yahoo.com>
2026-01-09 23:35:24 -0600

    make web server port configurable via arg for easier debugging

--- BattleshipWebServer/main.cpp
+++ BattleshipWebServer/main.cpp
@@ -1,8 +1,9 @@
 #include "WsGameController.h"
 #include <drogon/drogon.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+    int port = argc >= 2 ? std::stoi(argv[1]) : 8080; //allow port selection as runtime arg, default to 8080
     auto& app = drogon::app();
-    app.addListener("127.0.0.1", 8080);
+    app.addListener("127.0.0.1", port);
     app.run();
 }
