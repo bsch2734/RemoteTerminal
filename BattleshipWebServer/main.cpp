88c1f05a9f0195613715691cb9f71692da93f624
bsch2734 <bsch2734@yahoo.com>
2026-01-10 19:57:44 -0600

    set backend game request handling logic to single threaded to prevent concurency issues

--- BattleshipWebServer/main.cpp
+++ BattleshipWebServer/main.cpp
@@ -5,5 +5,6 @@ int main(int argc, char* argv[]) {
     int port = argc >= 2 ? std::stoi(argv[1]) : 8080; //allow port selection as runtime arg, default to 8080
     auto& app = drogon::app();
     app.addListener("127.0.0.1", port);
+	app.setThreadNum(1); //single-threaded to avoid concurrency issues in game logic
     app.run();
 }
