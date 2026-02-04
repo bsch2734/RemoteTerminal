c85c2bc2d6f42608a9221ef0348c110710d90b4e
bsch2734 <bsch2734@yahoo.com>
2026-02-04 00:49:23 -0600

    remove drogon local host filter so files can actually be served

--- RemoteTerminalWebServer/GamesHttpController.h
+++ RemoteTerminalWebServer/GamesHttpController.h
@@ -5,7 +5,7 @@
 class GamesHttpController : public drogon::HttpController<GamesHttpController> {
 public:
     METHOD_LIST_BEGIN
-        ADD_METHOD_TO(GamesHttpController::listGames, "/api/games", drogon::Get, "drogon::LocalHostFilter");
+        ADD_METHOD_TO(GamesHttpController::listGames, "/api/games", drogon::Get);
     METHOD_LIST_END
 
     void listGames(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
